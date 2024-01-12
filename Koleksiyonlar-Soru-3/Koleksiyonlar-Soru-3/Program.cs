464807d9350f8e6d8921dcde4792dd1322b1238f
Ayşe <79402056+ayseyalcinkaya@users.noreply.github.com>
2024-01-12 22:03:37 +0300

    Add files via upload

---
+++ Koleksiyonlar-Soru-3/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar_Soru_3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+            {
+                Console.WriteLine("Bir cümle giriniz:");
+                string cumle = Console.ReadLine().ToLower(); 
+
+                char[] sesliHarfler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+                List<char> bulunanSesliler = new List<char>();
+
+                foreach (char harf in cumle)
+                {
+                    if (Array.IndexOf(sesliHarfler, harf) >= 0)
+                    {
+                        bulunanSesliler.Add(harf);
+                    }
+                }
+
+                bulunanSesliler.Sort(); 
+
+                Console.WriteLine("Cümledeki sesli harfler:");
+                foreach (char sesli in bulunanSesliler)
+                {
+                    Console.Write(sesli + " ");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
