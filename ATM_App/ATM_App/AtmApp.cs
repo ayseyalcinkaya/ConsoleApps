b304111db4584ef320273201ddf7366ad42b74c9
Ayşe <79402056+ayseyalcinkaya@users.noreply.github.com>
2024-01-19 20:46:00 +0300

    Add files via upload
    Uygulama ilk çalıştığında kullanıcıdan yamak istediği işlemi öğrenmelidir. Bunlar ATM üzerinden yapılabilecek temem işlemledir. Para çekme, para yatırma, ödeme yapma vs.. İsteğe bağlı olarak genişletilebilir. Öncelikle ATM ye giriş yapan kullanıcın sistemde kayıtlı bir kullanıcı olduğundan emin olunmalıdır.
    
    Uygulamada bir de gün sonu seçeneği olmalıdır. Gün sonu alınmak istendiğinde, gün içerisinde yapılan transaction ların logları ve fraud olabilecek yani hatalı giriş denemeleri log gösterilmelidir. Aynı client'ın bilgisayarında belirlenen bir lokasyona EOD_Tarih(DDMMYYY formatında).txt adında bir dosyaya yazılmalıdır.
    
    Kullanılması gereken teknikler:
    
    Dosyaya Yazma
    Dosyadan Okuma
    İşlem listesi pre-defined olarak kullanılabilir.

---
+++ ATM_App/ATM_App/AtmApp.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class AtmApp
+{
+    private Dictionary<string, string> userAccounts; 
+    private List<string> transactionLogs; 
+    private string eodFilePath; 
+
+    public AtmApp()
+    {
+        userAccounts = new Dictionary<string, string>();
+        transactionLogs = new List<string>();
+        eodFilePath = "EOD_" + DateTime.Now.ToString("ddMMyyyy") + ".txt";
+
+        userAccounts.Add("1234567890", "1234"); 
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("ATM Uygulamasına Hoşgeldiniz!");
+
+        if (ValidateUser())
+        {
+            ShowMainMenu();
+        }
+        else
+        {
+            Console.WriteLine("Hatalı giriş denemesi!");
+        }
+    }
+
+    private bool ValidateUser()
+    {
+        Console.WriteLine("Kart numaranızı girin:");
+        string cardNumber = Console.ReadLine();
+
+        Console.WriteLine("PIN'inizi girin:");
+        string pin = Console.ReadLine();
+
+        if (userAccounts.ContainsKey(cardNumber))
+        {
+            return userAccounts[cardNumber] == pin;
+        }
+        else
+        {
+            Console.WriteLine("Kart numarası sistemde kayıtlı değil.");
+            return false;
+        }
+    }
+
+    private void ShowMainMenu()
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("Ana Menü:");
+            Console.WriteLine("1. Para Çekme");
+            Console.WriteLine("2. Para Yatırma");
+            Console.WriteLine("3. Ödeme Yapma");
+            Console.WriteLine("4. Gün Sonu İşlemleri");
+            Console.WriteLine("5. Çıkış");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Çekmek istediğiniz miktarı girin:");
+                    decimal amount = Convert.ToDecimal(Console.ReadLine());
+                    WithdrawMoney(amount);
+                    break;
+                case 2:
+                    Console.WriteLine("Para başarıyla yatırıldı.");
+                    break;
+                case 3:
+                    Console.WriteLine("Ödeme başarıyla yapıldı.");
+                    break;
+                case 4:
+                    ProcessEOD();
+                    break;
+                case 5:
+                    exit = true; // Çıkış seçeneği, döngüyü sonlandırır
+                    break;
+                default:
+                    Console.WriteLine("Geçersiz seçim!");
+                    break;
+            }
+        }
+        Console.Read();
+    }
+
+    private void ProcessEOD()
+    {
+
+        File.WriteAllLines(eodFilePath, transactionLogs);
+        Console.WriteLine("Gün sonu işlemleri kaydedildi: " + eodFilePath);
+    }
+
+    private void WithdrawMoney(decimal amount)
+    {
+
+        string logEntry = $"Para Çekme: {amount} çekildi. Tarih: {DateTime.Now}";
+
+        transactionLogs.Add(logEntry);
+
+        Console.WriteLine($"{amount} başarıyla çekildi.");
+    }
+}
---
+++ ATM_App/ATM_App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AtmApp atmApp = new AtmApp();
+            atmApp.Start();
+        }
+    }
+}
