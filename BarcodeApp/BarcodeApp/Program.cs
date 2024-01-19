c9f1d5ffb71b86897a4b54f52fe1725c1fe7c797
Ayşe <79402056+ayseyalcinkaya@users.noreply.github.com>
2024-01-19 20:03:44 +0300

    Add files via upload
    Barcode Generator/Reader
    3rd party barcode kütüphaneleri kullanarak barcode üreten bunu client'ın bilgisayarında bir lokasyona kaydeden. Daha sonra da barcode'u okuyabilen bir console uygulaması yazınız.
    Kullanılması gereken teknikler:
    Console Application
    Import 3rd Party Library
    Dosyaya Yazma
    Dosyadan Okuma

---
+++ BarcodeApp/BarcodeApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using ZXing;
+using System.Drawing.Imaging;
+using System.Drawing;
+using Image = System.Drawing.Image;
+
+namespace BarcodeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.CODE_128,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Height = 100,
+                    Width = 300
+                }
+            };
+
+            Console.Write("Barkod için metin girin: ");
+            string barcodeText = Console.ReadLine();
+            var barcodeBitmap = writer.Write(barcodeText);
+
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "barcode.png");
+            barcodeBitmap.Save(path, ImageFormat.Png);
+            Console.WriteLine($"Barkod, masaüstünde 'barcode.png' olarak kaydedildi: {path}");
+
+
+            var reader = new BarcodeReader();
+            var barcodeBitmapRead = (Bitmap)Image.FromFile(path);
+            var result = reader.Decode(barcodeBitmapRead);
+            if (result != null)
+            {
+                Console.WriteLine($"Barkod içeriği: {result.Text}");
+            }
+            else
+            {
+                Console.WriteLine("Barkod okunamadı.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
