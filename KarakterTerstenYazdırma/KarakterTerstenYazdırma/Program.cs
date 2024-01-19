a2d91d64c169c1524237b251bb405e0bce7860d4
Ayşe <79402056+ayseyalcinkaya@users.noreply.github.com>
2024-01-19 19:25:07 +0300

    Add files via upload
    Verilen string ifade içerisindeki karakterleri bir önceki karakter ile yer değiştiren console uygulamasını yazınız.
    Örnek: Input: Merhaba Hello Question
    Output: erhabaM elloH uestionQ

---
+++ KarakterTerstenYazdırma/KarakterTerstenYazdırma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarakterTerstenYazdırma
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Lütfen bir metin giriniz:");
+            string input = Console.ReadLine();
+
+            string output = KarakterlerinYerleriniDegistir(input);
+
+            Console.WriteLine("Sonuç:");
+            Console.WriteLine(output);
+            Console.Read();
+        }
+
+        static string KarakterlerinYerleriniDegistir(string input)
+        {
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > 1)
+                {
+                    words[i] = words[i].Substring(1) + words[i][0];
+                }
+            }
+            return String.Join(" ", words);
+        }
+    }
+}
