be0e2a1305fd2c3b121eff943612b0df823e7fbe
Ayşe <79402056+ayseyalcinkaya@users.noreply.github.com>
2024-01-19 14:26:58 +0300

    Add files via upload
    Ekrandan girilen n tane sayının 67'den küçük yada büyük olduğuna bakan. Küçük olanların farklarının toplamını, büyük ise de farkların mutlak karelerini alarak toplayıp ekrana yazdıran console uygulamasını yazınız.
    Örnek: Input: 56 45 68 77
    Output: 33 101

---
+++ Algorithm_3/Algorithm_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen sayıları giriniz (örn: 56 45 68 77):");
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            int küçükToplam = 0;
+            int büyükToplam = 0;
+
+            foreach (var number in numbers)
+            {
+                int num = int.Parse(number);
+
+                if (num < 67)
+                {
+                    küçükToplam += 67 - num;
+                }
+                else if (num > 67)
+                {
+                    büyükToplam += (num - 67) * (num - 67);
+                }
+            }
+
+            Console.WriteLine($"67 Sayısından küçük olanların Toplam Farkı: {küçükToplam}");
+            Console.WriteLine($"67 Sayısından büyük olanların Farklarının Kareler Toplamı: {büyükToplam}");
+
+
+            Console.Read();
+
+
+
+        }
+    }
+}
