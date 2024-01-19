d5caa2d2b7c6d08c839b72d034d51a6c1a446db5
Ayşe <79402056+ayseyalcinkaya@users.noreply.github.com>
2024-01-19 15:13:28 +0300

    Add files via upload
    Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulamasını yazınız.
    Örnek: Input: Merhaba Umut Arya
    Output: True False True

---
+++ SessizHarf/SessizHarf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SessizHarf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir ifade giriniz (örn: Merhaba Umut Arya):");
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+
+            foreach (var word in words)
+            {
+                Console.Write(SessizHarfKontrol(word) + " ");
+            }
+
+            Console.Read();
+        }
+
+        static bool SessizHarfKontrol(string word)
+        {
+            string consonants = "bcçdfgğhjklmnprsştvyzBCÇDFGĞHJKLMNPRSŞTVYZ";
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (consonants.Contains(word[i]) && consonants.Contains(word[i + 1]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
